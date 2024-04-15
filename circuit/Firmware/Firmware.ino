b6c7ba62e6e26abb3b86e41f4c5d1139e2e65d09
Shantanu P <shantanup410@gmail.com>
2024-04-15 16:03:50 +0530

    added code to send data

--- circuit/Firmware/Firmware.ino
+++ circuit/Firmware/Firmware.ino
@@ -1,3 +1,5 @@
+double calorie_index = 0.05;
+double mainCal = 0;
 
 // Include Libraries
 #include "Arduino.h"
@@ -7,19 +9,23 @@
 #include "I2Cdev.h"
 #include "Button.h"
 
+#include <SPI.h>
+#include "RF24.h"
+#include "nRF24L01.h"
+
 
 // Pin Definitions
-#define HEARTPULSE_PIN_SIG	A0
-#define NRF24L01_PIN_CE	2
-#define NRF24L01_PIN_CS	10
-#define PUSHBUTTON_1_PIN_2	3
-#define PUSHBUTTON_2_PIN_2	4
-#define PUSHBUTTON_3_PIN_2	5
-#define PUSHBUTTON_4_PIN_2	6
-#define PUSHBUTTON_5_PIN_2	7
-#define PUSHBUTTON_6_PIN_2	8
-#define PUSHBUTTON_7_PIN_2	9
-
+#define HEARTPULSE_PIN_SIG A0
+#define NRF24L01_PIN_CE 2
+#define NRF24L01_PIN_CS 10
+#define PUSHBUTTON_1_PIN_2 3
+#define PUSHBUTTON_2_PIN_2 4
+#define PUSHBUTTON_3_PIN_2 5
+#define PUSHBUTTON_4_PIN_2 6
+#define PUSHBUTTON_5_PIN_2 7
+#define PUSHBUTTON_6_PIN_2 8
+#define PUSHBUTTON_7_PIN_2 9
+#define PUSHBUTTON_8_PIN_2	A1
 
 
 // Global variables and defines
@@ -35,248 +41,244 @@ Button pushButton_4(PUSHBUTTON_4_PIN_2);
 Button pushButton_5(PUSHBUTTON_5_PIN_2);
 Button pushButton_6(PUSHBUTTON_6_PIN_2);
 Button pushButton_7(PUSHBUTTON_7_PIN_2);
+Button pushButton_8(PUSHBUTTON_8_PIN_2);
 
 
 // define vars for testing menu
-const int timeout = 10000;       //define timeout of 10 sec
+const int timeout = 10000;  //define timeout of 10 sec
 char menuOption = 0;
 long time0;
 
-// Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
-void setup() 
-{
-    // Setup Serial which is useful for debugging
-    // Use the Serial Monitor to view printed messages
-    Serial.begin(9600);
-    while (!Serial) ; // wait for serial port to connect. Needed for native USB
-    Serial.println("start");
-    
-    heartpulse.begin(HEARTPULSE_PIN_SIG);
-    Wire.begin();
-    mpu6050.initialize();
-    pushButton_1.init();
-    pushButton_2.init();
-    pushButton_3.init();
-    pushButton_4.init();
-    pushButton_5.init();
-    pushButton_6.init();
-    pushButton_7.init();
-    menuOption = menu();
-    
+RF24 radio(NRF24L01_PIN_CE, NRF24L01_PIN_CS);  // CE, CSN
+const byte address[6] = "00001";
+
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  Serial.println("start");
+
+  heartpulse.begin(HEARTPULSE_PIN_SIG);
+  Wire.begin();
+  mpu6050.initialize();
+
+  pushButton_1.init();
+  pushButton_2.init();
+  pushButton_3.init();
+  pushButton_4.init();
+  pushButton_5.init();
+  pushButton_6.init();
+  pushButton_7.init();
+  pushButton_8.init();
+
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+  //menuOption = menu();
 }
 
 // Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
-void loop() 
-{
-    
-    
-    if(menuOption == '1') {
-    // Heart Rate Pulse Sensor - Test Code
-    //Measure Heart Rate
-    int heartpulseBPM = heartpulse.BPM;
-    Serial.println(heartpulseBPM);
-    if (heartpulse.QS == true) {
+void loop() {
+  sendData();
+  delay(1000);
+}
+
+char menu() {
+
+  Serial.println(F("\nWhich component would you like to test?"));
+  Serial.println(F("(0) calories"));
+  Serial.println(F("(1) Heart Rate Pulse Sensor"));
+  Serial.println(F("(2) SparkFun MPU-6050 - Accelerometer and Gyro"));
+  Serial.println(F("(3) NRF24L01 - 2.4G Wireless Transceiver Module"));
+  Serial.println(F("(4) Mini Pushbutton Switch #1"));
+  Serial.println(F("(5) Mini Pushbutton Switch #2"));
+  Serial.println(F("(6) Mini Pushbutton Switch #3"));
+  Serial.println(F("(7) Mini Pushbutton Switch #4"));
+  Serial.println(F("(8) Mini Pushbutton Switch #5"));
+  Serial.println(F("(9) Mini Pushbutton Switch #6"));
+  Serial.println(F("(10) Mini Pushbutton Switch #7"));
+  Serial.println(F("(menu) send anything else or press on board reset button\n"));
+  while (!Serial.available())
+    ;
+
+  // Read data from serial monitor if received
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (isAlphaNumeric(c)) {
+
+      if (c == '0')
+        Serial.println(F("Now counting calories"));
+      else if (c == '1')
+        Serial.println(F("Now Testing Heart Rate Pulse Sensor"));
+      else if (c == '2')
+        Serial.println(F("Now Testing SparkFun MPU-6050 - Accelerometer and Gyro"));
+      else if (c == '3')
+        Serial.println(F("Now Testing NRF24L01 - 2.4G Wireless Transceiver Module - note that this component doesn't have a test code"));
+      else if (c == '4')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #1"));
+      else if (c == '5')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #2"));
+      else if (c == '6')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #3"));
+      else if (c == '7')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #4"));
+      else if (c == '8')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #5"));
+      else if (c == '9')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #6"));
+      else if (c == '10')
+        Serial.println(F("Now Testing Mini Pushbutton Switch #7"));
+      else {
+        Serial.println(F("illegal input!"));
+        return 0;
+      }
+      time0 = millis();
+      return c;
+    }
+  }
+}
+
+
+
+int heartBeats() {
+  return heartpulse.BPM;
+}
+
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+int minVal = 265;
+int maxVal = 402;
+
+double x;
+double y;
+double z;
+
+void readVelo() {
+  mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);  //read accelerometer and gyroscope raw data in three axes
+
+  int xAng = map(AcX, minVal, maxVal, -90, 90);
+  int yAng = map(AcY, minVal, maxVal, -90, 90);
+  int zAng = map(AcZ, minVal, maxVal, -90, 90);
+
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+
+  Serial.print("AngleX= ");
+  Serial.println(x);
+
+  Serial.print("AngleY= ");
+  Serial.println(y);
+
+  Serial.print("AngleZ= ");
+  Serial.println(z);
+  Serial.println("----------");
+}
+
+
+double return_positive(double val1, double val2) {
+  double tmp = val1 - val2;
+  if (tmp < 0) {
+    return (tmp * -1.0);
+  } else {
+    return (tmp);
+  }
+}
+
+double calculate_cal(double diff) {
+  return ((diff * calorie_index) / 360);
+}
+
+double getCal() {
+  mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
+
+  int xAng = map(AcX, minVal, maxVal, -90, 90);
+  int yAng = map(AcY, minVal, maxVal, -90, 90);
+  int zAng = map(AcZ, minVal, maxVal, -90, 90);
+
+  double xin = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  double yin = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  double zin = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+
+  // calculations
+  double diffX = return_positive(xin, x);
+  double diffY = return_positive(yin, y);
+  double diffZ = return_positive(zin, z);
+
+  double calo = calculate_cal(diffX) + calculate_cal(diffY) + calculate_cal(diffZ);
+
+  // assign
+  x = xin;
+  y = yin;
+  z = zin;
+
+  return calo;
+}
+
+void countCal(){
+  mainCal += getCal();
+  Serial.println(mainCal);
+  delay(300);
+}
+
+int countBPM(){
+  int heartpulseBPM = heartpulse.BPM;
+  if (heartpulse.QS == true) {
     Serial.println("PULSE");
     heartpulse.QS = false;
-    }
-    }
-    else if(menuOption == '2') {
-    // SparkFun MPU-6050 - Accelerometer and Gyro - Test Code
-    mpu6050.getMotion6(&mpu6050Ax, &mpu6050Ay, &mpu6050Az, &mpu6050Gx, &mpu6050Gy, &mpu6050Gz);   //read accelerometer and gyroscope raw data in three axes
-    double mpu6050Temp = ((double)mpu6050.getTemperature() + 12412.0) / 340.0;
-    Serial.print("a/g-\t");
-    Serial.print(mpu6050Ax); Serial.print("\t");
-    Serial.print(mpu6050Ay); Serial.print("\t");
-    Serial.print(mpu6050Az); Serial.print("\t");
-    Serial.print(mpu6050Gx); Serial.print("\t");
-    Serial.print(mpu6050Gy); Serial.print("\t");
-    Serial.print(mpu6050Gz); Serial.print("\t");
-    Serial.print(F("Temp- "));   
-    Serial.println(mpu6050Temp);
-    delay(100);
-
-    }
-    else if(menuOption == '3')
-    {
-    // Disclaimer: The NRF24L01 - 2.4G Wireless Transceiver Module is in testing and/or doesn't have code, therefore it may be buggy. Please be kind and report any bugs you may find.
-    }
-    else if(menuOption == '4') {
-    // Mini Pushbutton Switch #1 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_1.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_1Val = pushButton_1.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_1Val);
-
-    }
-    else if(menuOption == '5') {
-    // Mini Pushbutton Switch #2 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_2.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_2Val = pushButton_2.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_2Val);
-
-    }
-    else if(menuOption == '6') {
-    // Mini Pushbutton Switch #3 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_3.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_3Val = pushButton_3.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_3Val);
-
-    }
-    else if(menuOption == '7') {
-    // Mini Pushbutton Switch #4 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_4.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_4Val = pushButton_4.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_4Val);
-
-    }
-    else if(menuOption == '8') {
-    // Mini Pushbutton Switch #5 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_5.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_5Val = pushButton_5.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_5Val);
-
-    }
-    else if(menuOption == '9') {
-    // Mini Pushbutton Switch #6 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_6.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_6Val = pushButton_6.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_6Val);
-
-    }
-    else if(menuOption == '10') {
-    // Mini Pushbutton Switch #7 - Test Code
-    //Read pushbutton state. 
-    //if button is pressed function will return HIGH (1). if not function will return LOW (0). 
-    //for debounce funtionality try also pushButton_7.onPress(), .onRelease() and .onChange().
-    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-    bool pushButton_7Val = pushButton_7.read();
-    Serial.print(F("Val: ")); Serial.println(pushButton_7Val);
-
-    }
-    
-    if (millis() - time0 > timeout)
-    {
-        menuOption = menu();
-    }
-    
+  }
+  return heartpulseBPM;
 }
 
+byte getTouch(){
+  bool pushButton_1Val = pushButton_1.read();
+  bool pushButton_2Val = pushButton_2.read();
+  bool pushButton_3Val = pushButton_3.read();
+  bool pushButton_4Val = pushButton_4.read();
+
+  bool pushButton_5Val = pushButton_5.read();
+  bool pushButton_6Val = pushButton_6.read();
+  bool pushButton_7Val = pushButton_7.read();
+  bool pushButton_8Val = pushButton_8.read();
 
 
-// Menu function for selecting the components to be tested
-// Follow serial monitor for instrcutions
-char menu()
+  byte vals = (pushButton_1Val | vals);
+  vals = (pushButton_2Val << 1 | vals);
+  vals = (pushButton_3Val << 2 | vals);
+  vals = (pushButton_4Val << 3 | vals);
+
+  vals = (pushButton_5Val << 4 | vals);
+  vals = (pushButton_6Val << 5 | vals);
+  vals = (pushButton_7Val << 6 | vals);
+  vals = (pushButton_8Val << 7 | vals);
+
+  // Serial.println(String(vals));
+
+  return vals;
+  
+}
+
+char* ConvertStringToCharArray(String S)
 {
+  int   ArrayLength  =S.length()+1;    //The +1 is for the 0x00h Terminator
+  char  CharArray[ArrayLength];
+  S.toCharArray(CharArray,ArrayLength);
 
-    Serial.println(F("\nWhich component would you like to test?"));
-    Serial.println(F("(1) Heart Rate Pulse Sensor"));
-    Serial.println(F("(2) SparkFun MPU-6050 - Accelerometer and Gyro"));
-    Serial.println(F("(3) NRF24L01 - 2.4G Wireless Transceiver Module"));
-    Serial.println(F("(4) Mini Pushbutton Switch #1"));
-    Serial.println(F("(5) Mini Pushbutton Switch #2"));
-    Serial.println(F("(6) Mini Pushbutton Switch #3"));
-    Serial.println(F("(7) Mini Pushbutton Switch #4"));
-    Serial.println(F("(8) Mini Pushbutton Switch #5"));
-    Serial.println(F("(9) Mini Pushbutton Switch #6"));
-    Serial.println(F("(10) Mini Pushbutton Switch #7"));
-    Serial.println(F("(menu) send anything else or press on board reset button\n"));
-    while (!Serial.available());
-
-    // Read data from serial monitor if received
-    while (Serial.available()) 
-    {
-        char c = Serial.read();
-        if (isAlphaNumeric(c)) 
-        {   
-            
-            if(c == '1') 
-    			Serial.println(F("Now Testing Heart Rate Pulse Sensor"));
-    		else if(c == '2') 
-    			Serial.println(F("Now Testing SparkFun MPU-6050 - Accelerometer and Gyro"));
-    		else if(c == '3') 
-    			Serial.println(F("Now Testing NRF24L01 - 2.4G Wireless Transceiver Module - note that this component doesn't have a test code"));
-    		else if(c == '4') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #1"));
-    		else if(c == '5') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #2"));
-    		else if(c == '6') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #3"));
-    		else if(c == '7') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #4"));
-    		else if(c == '8') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #5"));
-    		else if(c == '9') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #6"));
-    		else if(c == '10') 
-    			Serial.println(F("Now Testing Mini Pushbutton Switch #7"));
-            else
-            {
-                Serial.println(F("illegal input!"));
-                return 0;
-            }
-            time0 = millis();
-            return c;
-        }
-    }
+  return(CharArray);
 }
 
-/*******************************************************
 
-*    Circuito.io is an automatic generator of schematics and code for off
-*    the shelf hardware combinations.
+void sendData(){
+  char dataMed[25];
 
-*    Copyright (C) 2016 Roboplan Technologies Ltd.
+  String touchDataStr = String(getTouch()) + "," + String(heartpulse.BPM) + "," + String(getCal());
+  touchDataStr.toCharArray(dataMed,touchDataStr.length()+1);
+  //Serial.println(sizeof(dataMed));
 
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
+  radio.write(&dataMed, sizeof(dataMed));
 
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*    In addition, and without limitation, to the disclaimers of warranties 
-*    stated above and in the GNU General Public License version 3 (or any 
-*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
-*    program subject to the following warranty disclaimers and by using 
-*    this program you acknowledge and agree to the following:
-*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
-*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
-*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
-*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
-*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
-*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
-*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
-*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
-*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
-*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
-*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
-*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
-*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
-*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
-*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
-*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
-*    THE ABOVE. 
-********************************************************/
+}
---
+++ circuit/Firmware2/Firmware2.ino
@@ -0,0 +1,236 @@
+
+// Include Libraries
+#include "Arduino.h"
+#include "Buzzer.h"
+#include "LED.h"
+
+#include <SPI.h>
+#include "RF24.h"
+#include "nRF24L01.h"
+
+
+// Pin Definitions
+#define BUZZER_PIN_SIG	2
+#define LEDB_1_PIN_VIN	3
+#define LEDB_2_PIN_VIN	5
+#define LEDB_3_PIN_VIN	6
+#define LEDB_4_PIN_VIN	9
+#define LEDB_5_PIN_VIN	10
+#define NRF24L01_PIN_CE	4
+#define NRF24L01_PIN_CS	7
+
+
+
+// Global variables and defines
+
+// object initialization
+Buzzer buzzer(BUZZER_PIN_SIG);
+LED ledB_1(LEDB_1_PIN_VIN);
+LED ledB_2(LEDB_2_PIN_VIN);
+LED ledB_3(LEDB_3_PIN_VIN);
+LED ledB_4(LEDB_4_PIN_VIN);
+LED ledB_5(LEDB_5_PIN_VIN);
+
+
+// define vars for testing menu
+const int timeout = 10000;       //define timeout of 10 sec
+char menuOption = 0;
+long time0;
+
+
+RF24 radio(NRF24L01_PIN_CE, NRF24L01_PIN_CS); // CE, CSN
+const byte address[6] = "00001";
+
+
+
+// Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
+void setup() 
+{
+    // Setup Serial which is useful for debugging
+    // Use the Serial Monitor to view printed messages
+    Serial.begin(9600);
+    while (!Serial) ; // wait for serial port to connect. Needed for native USB
+    Serial.println("start");
+    
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+    menuOption = menu();
+    
+}
+
+// Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
+void loop() 
+{
+    
+    
+    if(menuOption == '1') {
+    // Buzzer - Test Code
+    // The buzzer will turn on and off for 500ms (0.5 sec)
+    buzzer.on();       // 1. turns on
+    delay(500);             // 2. waits 500 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    buzzer.off();      // 3. turns off.
+    delay(500);             // 4. waits 500 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }
+    else if(menuOption == '2') {
+    // LED - Basic Blue 5mm #1 - Test Code
+    // The LED will turn on and fade till it is off
+    for(int i=255 ; i> 0 ; i -= 5)
+    {
+        ledB_1.dim(i);                      // 1. Dim Led 
+        delay(15);                               // 2. waits 5 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }                                          
+    ledB_1.off();                        // 3. turns off
+    }
+    else if(menuOption == '3') {
+    // LED - Basic Blue 5mm #2 - Test Code
+    // The LED will turn on and fade till it is off
+    for(int i=255 ; i> 0 ; i -= 5)
+    {
+        ledB_2.dim(i);                      // 1. Dim Led 
+        delay(15);                               // 2. waits 5 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }                                          
+    ledB_2.off();                        // 3. turns off
+    }
+    else if(menuOption == '4') {
+    // LED - Basic Blue 5mm #3 - Test Code
+    // The LED will turn on and fade till it is off
+    for(int i=255 ; i> 0 ; i -= 5)
+    {
+        ledB_3.dim(i);                      // 1. Dim Led 
+        delay(15);                               // 2. waits 5 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }                                          
+    ledB_3.off();                        // 3. turns off
+    }
+    else if(menuOption == '5') {
+    // LED - Basic Blue 5mm #4 - Test Code
+    // The LED will turn on and fade till it is off
+    for(int i=255 ; i> 0 ; i -= 5)
+    {
+        ledB_4.dim(i);                      // 1. Dim Led 
+        delay(15);                               // 2. waits 5 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }                                          
+    ledB_4.off();                        // 3. turns off
+    }
+    else if(menuOption == '6') {
+    // LED - Basic Blue 5mm #5 - Test Code
+    // The LED will turn on and fade till it is off
+    for(int i=255 ; i> 0 ; i -= 5)
+    {
+        ledB_5.dim(i);                      // 1. Dim Led 
+        delay(15);                               // 2. waits 5 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    }                                          
+    ledB_5.off();                        // 3. turns off
+    }
+    else if(menuOption == '7')
+    {
+    // Disclaimer: The NRF24L01 - 2.4G Wireless Transceiver Module is in testing and/or doesn't have code, therefore it may be buggy. Please be kind and report any bugs you may find.
+    if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    }
+    }
+    
+    if (millis() - time0 > timeout)
+    {
+        menuOption = menu();
+    }
+    
+}
+
+
+
+// Menu function for selecting the components to be tested
+// Follow serial monitor for instrcutions
+char menu()
+{
+
+    Serial.println(F("\nWhich component would you like to test?"));
+    Serial.println(F("(1) Buzzer"));
+    Serial.println(F("(2) LED - Basic Blue 5mm #1"));
+    Serial.println(F("(3) LED - Basic Blue 5mm #2"));
+    Serial.println(F("(4) LED - Basic Blue 5mm #3"));
+    Serial.println(F("(5) LED - Basic Blue 5mm #4"));
+    Serial.println(F("(6) LED - Basic Blue 5mm #5"));
+    Serial.println(F("(7) NRF24L01 - 2.4G Wireless Transceiver Module"));
+    Serial.println(F("(menu) send anything else or press on board reset button\n"));
+    while (!Serial.available());
+
+    // Read data from serial monitor if received
+    while (Serial.available()) 
+    {
+        char c = Serial.read();
+        if (isAlphaNumeric(c)) 
+        {   
+            
+            if(c == '1') 
+    			Serial.println(F("Now Testing Buzzer"));
+    		else if(c == '2') 
+    			Serial.println(F("Now Testing LED - Basic Blue 5mm #1"));
+    		else if(c == '3') 
+    			Serial.println(F("Now Testing LED - Basic Blue 5mm #2"));
+    		else if(c == '4') 
+    			Serial.println(F("Now Testing LED - Basic Blue 5mm #3"));
+    		else if(c == '5') 
+    			Serial.println(F("Now Testing LED - Basic Blue 5mm #4"));
+    		else if(c == '6') 
+    			Serial.println(F("Now Testing LED - Basic Blue 5mm #5"));
+    		else if(c == '7') 
+    			Serial.println(F("Now Testing NRF24L01 - 2.4G Wireless Transceiver Module - note that this component doesn't have a test code"));
+            else
+            {
+                Serial.println(F("illegal input!"));
+                return 0;
+            }
+            time0 = millis();
+            return c;
+        }
+    }
+}
+
+/*******************************************************
+
+*    Circuito.io is an automatic generator of schematics and code for off
+*    the shelf hardware combinations.
+
+*    Copyright (C) 2016 Roboplan Technologies Ltd.
+
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*    In addition, and without limitation, to the disclaimers of warranties 
+*    stated above and in the GNU General Public License version 3 (or any 
+*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
+*    program subject to the following warranty disclaimers and by using 
+*    this program you acknowledge and agree to the following:
+*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
+*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
+*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
+*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
+*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
+*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
+*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
+*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
+*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
+*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
+*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
+*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
+*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
+*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
+*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
+*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
+*    THE ABOVE. 
+********************************************************/
